d09ef807b2c5aa67b66f53a55f7bb0c2008de0ac
robertpepelnjak <rpepelnjak@gmail.com>
2022-05-30 12:35:33 +0200

    skrajšal kodo

--- koreni1.py
+++ koreni1.py
@@ -5,14 +5,11 @@ if metoda.upper() == "T":
   x = int(input("Zapiši začetno število:\n"))
   rešitve = [x]
   while True:
-    xn = x
     xn = (x ** 2 + število)/(2*x)
     rešitve.append(xn)
     if round(xn, 10) == round(x, 10):
       break
     else:
       x = xn
-      xn = (x ** 2 + število)/(2*x)
-      rešitve.append(xn)
   for i in rešitve:
     print(i)

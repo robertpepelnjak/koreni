164cb9456035afce97a1bd67a1e09115a5ef08bb
robertpepelnjak <101881882+robertpepelnjak@users.noreply.github.com>
2022-05-30 12:02:06 +0200

    olepšal kodo (#3)

--- koreni1.py
+++ koreni1.py
@@ -1,17 +1,18 @@
+
 število = int(input("Koren katerega števila hočeš izračunati?\n"))
 metoda = str(input("S katero metodo hočeš?\nČe hočeš z bisekcijo napiši B, če hočeš s tangentno metodo napiši T \n"))
 if metoda.upper() == "T":
   x = int(input("Zapiši začetno število:\n"))
   rešitve = [x]
-  for number in range(2):
-    xn = (x ** 2 + število)/(2*x)
-    x = xn
-    rešitve.append(xn)
-    n = int(0)
-  while rešitve[n] != rešitve[n-2]:
-    x=xn
+  while True:
+    xn = x
     xn = (x ** 2 + število)/(2*x)
     rešitve.append(xn)
-    n+=1
-  for število in rešitve:
-    print(število)
+    if round(xn, 10) == round(x, 10):
+      break
+    else:
+      x = xn
+      xn = (x ** 2 + število)/(2*x)
+      rešitve.append(xn)
+  for i in rešitve:
+    print(i)
